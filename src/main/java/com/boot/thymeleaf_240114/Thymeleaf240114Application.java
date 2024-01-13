3564c0a2698ec8c28ffa95dd76ccebb14d281e49
carlton777 <sangkoo@naver.com>
2024-01-13 14:51:01 +0900

    init

---
+++ src/main/java/com/boot/thymeleaf_240114/Thymeleaf240114Application.java
@@ -0,0 +1,13 @@
+package com.boot.thymeleaf_240114;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thymeleaf240114Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Thymeleaf240114Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/boot/thymeleaf_240114/Thymeleaf240114ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.thymeleaf_240114;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Thymeleaf240114ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
